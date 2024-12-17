c8c933031021c4f3ddf61ad349d29f345c0ec4c8
hugopbx <hugo@backpack.exchange>
2024-12-17 11:56:58 +0000

    feat: support rkyv 0.8

--- src/decimal.rs
+++ src/decimal.rs
@@ -108,10 +108,8 @@ pub struct UnpackedDecimal {
 #[cfg_attr(
     feature = "rkyv",
     derive(Archive, Deserialize, Serialize),
-    archive(compare(PartialEq)),
-    archive_attr(derive(Clone, Copy, Debug))
+    rkyv(compare(PartialEq), derive(Clone, Copy, Debug))
 )]
-#[cfg_attr(feature = "rkyv-safe", archive(check_bytes))]
 pub struct Decimal {
     // Bits 0-15: unused
     // Bits 16-23: Contains "e", a value between 0-28 that indicates the scale
